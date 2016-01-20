b3b641a175f387cb3ebc6ebba2d983730319013a
egza <iegor.zakharov@gmail.com>
2016-01-20 12:51:43 +0200

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,6 @@
 module.exports = function(grunt) {
   var path = require('path');
 
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.initConfig({
-      jshint: {
-        all: ['Gruntfile.js', '**/*.js']
-      },
-      watch: {
-        files: ['Gruntfile.js', '**/*.js'],
-        tasks: ['jshint']
-    }
-  });
-
   require('load-grunt-config')(grunt, {
     configPath: path.join(process.cwd(), 'grunt/config'),
     jitGrunt: {
